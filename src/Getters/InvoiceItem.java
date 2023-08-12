a7bd510b8850a022dc720ea5bd9921e98b175525
srijithgouri <srijithgouri2@gmail.com>
2023-08-12 22:31:43 +0530

    added new lines

--- src/Getters/InvoiceItem.java
+++ src/Getters/InvoiceItem.java
@@ -1,19 +1,23 @@
 package Getters;
 
 public class InvoiceItem {
+	
 private String id;
 private String desc;
 private int qty;
 private double unitPrice;
+
 public InvoiceItem() {
 	
 }
+
 public InvoiceItem(String id,String desc,int qty,double unitPrice) {
 	this.id=id;
 	this.desc=desc;
 	this.qty=qty;
 	this.unitPrice=unitPrice;
 }
+
 public String getId() {
 	return id;
 }
